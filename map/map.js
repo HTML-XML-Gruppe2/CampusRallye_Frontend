597deccadc744a986a79c0285674254c0affa40d
KnapheideSi <42971654+KnapheideSi@users.noreply.github.com>
2018-10-20 10:46:11 +0200

    Seperated js vom html

--- map/map.js
+++ map/map.js
@@ -1,330 +1,223 @@
-$(document).ready(function () {
-    $("#navButton").click(function(){
-        $("#sideMenu").css("display", "block");
-    });
-    $("#closeMenu").click(function(){
-        $("#sideMenu").css("display", "none");
-    });
-});
+var requestURL = "https://localhost:8080/api/objects/basic";
+var Markers = new Array();
+// Map sprite
+var mapSprite = new Image();
+var canvas = document.getElementById('Canvas');
+var context = canvas.getContext("2d");
+mapSprite.src = "map.jpg";
+//Größenverhältnis
+var sizeheight = mapSprite.naturalHeight / mapSprite.naturalWidth;
+// VIsuelle AUsfüllung des Elternobjektes
+canvas.style.width = '100%';
+canvas.style.height = canvas.style.width * sizeheight;
+// ...setzen der internen Größe
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.width * sizeheight;
 
-//Knapheidesi Skript eingefügt https://www.softwarecountry.com/articles/interactive-map-of-shopping-mall-on-html5-canvas/
 
-var element = $('#canvas'), // we are going to use it for event handling
-canvas = new fabric.Canvas(element.get(0), {
-        selection: false, // disable groups selection
-        scale: 1, // set default scale
-	renderOnAddRemove: false, // disable auto-render in order to improve performance for complex maps with a lot of markers
-	moveCursor: 'default', // reset mouse cursor - they are not used by us
-	hoverCursor: 'default'
-});
+//START:Knapheidesi Karte Responsive an Fenster anpassen
+window.addEventListener("resize", handleResize);
+function handleResize() {
+	// Make it visually fill the positioned parent
+	canvas.style.width = '100%';
+	canvas.style.height = canvas.style.width * sizeheight;
+	// ...then set the internal size to match
+	canvas.width = canvas.offsetWidth;
+	canvas.height = canvas.width * sizeheight;
+	main();
+}
+window.addEventListener('orientationchange', handleResize);
+//END:Knapheidesi Karte Responsive an Fenster anpassen
 
-var baseWidth = 0,
-	baseHeight= 0,
-	baseScale = 1,
-        width = 0, // current width
-	height = 0, // current height
-	transX = 0, // current shift for x axis
-	transY = 0, // current shift for y axis
-	scale = 1; // current global scale
-	
-var applyTransform = function () {
-	var maxTransX,
-		maxTransY,
-		minTransX,
-		minTransY,
-		group;
+//START:Knapheidesi Zum testen einen Marker zu bearbeitet hinzugefügt
+// var visitedObjectsCampusRallye = localStorage.getItem("visitedObjectsCampusRallye");
+// var visitedObjectsCampusRallyeJSON = JSON.parse(visitedObjectsCampusRallye);
+// visitedObjectsCampusRallyeJSON['visitedObjects'].push({ "_id": '5bc7a042113fa655f46a8a3e', "name": 'Test' });
+// visitedObjectsCampusRallye = JSON.stringify(visitedObjectsCampusRallyeJSON);
+// localStorage.setItem("visitedObjectsCampusRallye", visitedObjectsCampusRallye);
+//END:Knapheidesi Zum testen einen Marker zu bearbeitet hinzugefügt
 
-	// Calculation of thresholds for x axis transition
-	if (baseWidth * scale <= width) {
-		// Map fits into the canvas
-		maxTransX = (width - baseWidth * scale) / (2 * scale);
-		minTransX = (width - baseWidth * scale) / (2 * scale);
-	} else {
-		// Map doesn’t fit
-		maxTransX = 0;
-		minTransX = (width - baseWidth * scale) / scale;
-	}
+// function hammerIt(elm) {
+//     hammertime = new Hammer(elm, {});
+//     hammertime.get('pinch').set({
+//         enable: true
+//     });
+//     var posX = 0,
+//         posY = 0,
+//         scale = 1,
+//         last_scale = 1,
+//         last_posX = 0,
+//         last_posY = 0,
+//         max_pos_x = 0,
+//         max_pos_y = 0,
+//         transform = "",
+//         el = elm;
 
-	// Limit transition with calculated thresholds
-	if (transX > maxTransX) {
-		transX = maxTransX;
-	} else if (transX < minTransX) {
-		transX = minTransX;
-	}
+//     hammertime.on('doubletap pan pinch panend pinchend', function (ev) {
+//         if (ev.type == "doubletap") {
+//             transform =
+//                 "translate3d(0, 0, 0) " +
+//                 "scale3d(2, 2, 1) ";
+//             scale = 2;
+//             last_scale = 2;
+//             try {
+//                 if (window.getComputedStyle(el, null).getPropertyValue('-webkit-transform').toString() != "matrix(1, 0, 0, 1, 0, 0)") {
+//                     transform =
+//                         "translate3d(0, 0, 0) " +
+//                         "scale3d(1, 1, 1) ";
+//                     scale = 1;
+//                     last_scale = 1;
+//                 }
+//             } catch (err) { }
+//             el.style.webkitTransform = transform;
+//             transform = "";
+//         }
 
-	// The same for y axis
-	if (baseHeight * scale <= height) {
-		maxTransY = (height - baseHeight * scale) / (2 * scale);
-		minTransY = (height - baseHeight * scale) / (2 * scale);
-	} else {
-		maxTransY = 0;
-		minTransY = (height - baseHeight * scale) / scale;
-	}
-	if (transY > maxTransY) {
-		transY = maxTransY;
-	} else if (transY < minTransY) {
-		transY = minTransY;
-	}
+//         //pan    
+//         if (scale != 1) {
+//             posX = last_posX + ev.deltaX;
+//             posY = last_posY + ev.deltaY;
+//             max_pos_x = Math.ceil((scale - 1) * el.clientWidth / 2);
+//             max_pos_y = Math.ceil((scale - 1) * el.clientHeight / 2);
+//             if (posX > max_pos_x) {
+//                 posX = max_pos_x;
+//             }
+//             if (posX < -max_pos_x) {
+//                 posX = -max_pos_x;
+//             }
+//             if (posY > max_pos_y) {
+//                 posY = max_pos_y;
+//             }
+//             if (posY < -max_pos_y) {
+//                 posY = -max_pos_y;
+//             }
+//         }
+//         console.log(posY);
 
-	// Group all objects and apply transform on the group
-	group = new fabric.Group(canvas.getObjects());
-	group.scaleX = scale / canvas.scale;
-	group.scaleY = scale / canvas.scale;
-	group.left = group.getWidth() / 2 + transX * scale;
-	group.top = group.getHeight() / 2 + transY * scale;
-	group.destroy();
 
-	// Refresh global scale for the canvas
-	canvas.scale = scale;
+//         //pinch
+//         if (ev.type == "pinch") {
+//             scale = Math.max(.999, Math.min(last_scale * (ev.scale), 4));
+//         }
+//         if (ev.type == "pinchend") { last_scale = scale; }
 
-	// At last render canvas with changed objects
-	canvas.renderAll();
-};
+//         //panend
+//         if (ev.type == "panend") {
+//             last_posX = posX < max_pos_x ? posX : max_pos_x;
+//             last_posY = posY < max_pos_y ? posY : max_pos_y;
+//         }
 
-var setScale = function (scaleToSet, anchorX, anchorY) {
-	var zoomMax = 5, // max 5x zoom
-		zoomMin =  1, // min 1x zoom - real image size
-		zoomStep; // required scale change
-		
-	// Limit scale if needed
-	if (scaleToSet > zoomMax * baseScale) {
-		scaleToSet = zoomMax * baseScale;
-	} else if (scaleToSet < zoomMin * baseScale) {
-		scaleToSet = zoomMin * baseScale;
-	}
+//         if (scale != 1) {
+//             transform =
+//                 "translate3d(" + posX + "px," + posY + "px, 0) " +
+//                 "scale3d(" + scale + ", " + scale + ", 1)";
+//         }
 
-	// The center of zoom - a point which should stay on its place.
-	// It is defined by anchorX and anchorY arguments.
-	// In fact, it is just a position of mouse pointer during scaling.
-	if (typeof anchorX != 'undefined' && typeof anchorY != 'undefined') {
-		zoomStep = scaleToSet / scale;
-		// Calculate required shift for all objects
-		// so the center of zoom stays motionless.
-		transX -= (zoomStep - 1) / scaleToSet * anchorX;
-		transY -= (zoomStep - 1) / scaleToSet * anchorY;
-	}
+//         if (transform) {
+//             el.style.webkitTransform = transform;
+//         }
+//     });
+// }
 
-	scale = scaleToSet;	
-	applyTransform();
-};
 
-var bindContainerEvents= function () {
-	var mouseDown = false,
-		oldPageX,
-		oldPageY,
-		container = $(canvas.wrapperEl);
+//START:Knapheidesi Marker hinzufügen
+function getthemarkerdatat() {
 
-	container.mousemove(function (e) {
-		// Shifting
-		if (mouseDown) {
-			// Calculate transition with respect to the current scale
-			transX -= (oldPageX - e.pageX) / scale;
-			transY -= (oldPageY - e.pageY) / scale;
-
-			applyTransform();
-
-			oldPageX = e.pageX;
-			oldPageY = e.pageY;
-			return false;
+	// request information for ID from backend (data in contained in var object as JSON)
+	$.get(requestURL, function (object) {
+		for (var i = 0; i < object.length; i++) {
+			createMarker(object[i]._id, object[i].position.x, object[i].position.y);
 		}
-	}).mousedown(function (e) {
-		// Store position
-		mouseDown = true;
-		oldPageX = e.pageX;
-		oldPageY = e.pageY;
-		return false;
 	});
-
-	$('body').mouseup(function () {
-		mouseDown = false;
-	});
-
-	// Zoom with mouse wheel
-	container.mousewheel(function (event, delta, deltaX, deltaY) {
-		var offset = element.offset(), // position of the canvas on the page
-			centerX = event.pageX - offset.left, // x coordinate of the center of zoom 
-			centerY = event.pageY - offset.top, // y coordinate of the center of zoom 
-			zoomStep = Math.pow(1.3, deltaY); // user-friendly zoom step
-
-		setScale(scale * zoomStep, centerX, centerY);
-		
-		// Prevent scroll of the page
-		event.preventDefault();
-	});
-};
-
-var bindContainerTouchEvents = function () {
-	var touchStartScale,
-		touchStartDistance, 
-		container = $(canvas.wrapperEl),
-		touchX,
-		touchY,
-		centerTouchX,
-		centerTouchY,
-		lastTouchesLength,
-		handleTouchEvent = function (e) {
-			var touches = e.originalEvent.touches,
-				offset,
-				currentScale,
-				transXOld,
-				transYOld;
-
-			if (e.type == 'touchstart') {
-				lastTouchesLength = 0;
-			}
-			if (touches.length == 1) {
-				// Simple shift
-				if (lastTouchesLength == 1) {
-					transXOld = transX;
-					transYOld = transY;
-					transX -= (touchX - touches[0].pageX) / scale;
-					transY -= (touchY - touches[0].pageY) / scale;
-					applyTransform();
-					if (transXOld != transX || transYOld != transY) {
-						e.preventDefault();
-					}
-				}
-				touchX = touches[0].pageX;
-				touchY = touches[0].pageY;
-			} else if (touches.length == 2) {
-				// Zoom
-				if (lastTouchesLength == 2) {
-					currentScale = Math.sqrt(
-					  Math.pow(touches[0].pageX - touches[1].pageX, 2) +
-					  Math.pow(touches[0].pageY - touches[1].pageY, 2)
-					) / touchStartDistance;
-					setScale(touchStartScale * currentScale, centerTouchX, centerTouchY);
-					e.preventDefault();
-				} else {
-					// This is zoom start, store current state
-					offset = element.offset();
-					if (touches[0].pageX > touches[1].pageX) {
-						centerTouchX = touches[1].pageX + (touches[0].pageX - touches[1].pageX) / 2;
-					} else {
-						centerTouchX = touches[0].pageX + (touches[1].pageX - touches[0].pageX) / 2;
-					}
-					if (touches[0].pageY > touches[1].pageY) {
-						centerTouchY = touches[1].pageY + (touches[0].pageY - touches[1].pageY) / 2;
-					} else {
-						centerTouchY = touches[0].pageY + (touches[1].pageY - touches[0].pageY) / 2;
-					}
-					centerTouchX -= offset.left;
-					centerTouchY -= offset.top;
-					touchStartScale = scale;
-					touchStartDistance = Math.sqrt(
-					  Math.pow(touches[0].pageX - touches[1].pageX, 2) +
-					  Math.pow(touches[0].pageY - touches[1].pageY, 2)
-					);
+}
+var Marker = function () {
+	this.Sprite = new Image();
+	this.Sprite.src = "offenemarker.png";
+	this.Width = 12;
+	this.Height = 20;
+	this.XPos = 0;
+	this.YPos = 0;
+}
+function createMarker(OId, XPosi, YPosi) {
+	var marker = new Marker();
+	marker.XPos = XPosi;
+	marker.YPos = YPosi;
+	$.get(requestURL, function () {
+		// check if ID/object has already been processed (played by user) - data will be set in local storage
+		var visitedObjectsCampusRallye = localStorage.getItem("visitedObjectsCampusRallye");
+		var visitedObjectsCampusRallyeJSON = JSON.parse(visitedObjectsCampusRallye);
+		if (visitedObjectsCampusRallyeJSON !== null) {
+			for (n in visitedObjectsCampusRallyeJSON.visitedObjects) {
+				if (OId == visitedObjectsCampusRallyeJSON.visitedObjects[n]._id) {
+					marker.Sprite.src = "fertigemarker.png";
 				}
 			}
 
-			lastTouchesLength = touches.length;
-		};
 
-	container.bind('touchstart', handleTouchEvent);
-	container.bind('touchmove', handleTouchEvent);
-};
-
-var map = document.getElementById("Karte");
-fabric.util.loadImage('map.jpg', function(img) {
-	var map = new fabric.Image(img),
-		curBaseScale;
-	if (('ontouchstart' in window) || (window.DocumentTouch && document instanceof DocumentTouch)) {
-		bindContainerTouchEvents();
-	} else {
-		bindContainerEvents();
-	}
-	
-	// Set base and current dimensions
-	baseWidth = map.width;
-	baseHeight = map.height;
-	width = element.width();
-	height = element.height();
-	
-	// Disable all edit and object selecting functionality on the canvas
-	map.set({
-		hasRotatingPoint: false,
-		hasBorders: false,
-		hasControls: false,
-		lockScalingY: true,
-		lockScalingX: true,
-		selectable: false,
-		left: map.width / 2,
-		top: map.height / 2,
-		originX: 'center',
-		originY: 'center'
-	});
-	canvas.add(map);
-	
-	// Zoom after load in order to show whole map from the beginning
-	curBaseScale  = baseScale;
-	if (width / height > baseWidth / baseHeight) {
-		baseScale = height / baseHeight;
-	} else {
-		baseScale = width / baseWidth;
-	}
-	scale *= baseScale / curBaseScale;
-	transX *= baseScale / curBaseScale;
-	transY *= baseScale / curBaseScale;
-	
-	canvas.setWidth(width);
-	canvas.setHeight(height);
-	
-	applyTransform();
-	
-	// Show markers on the map, will be added later
-	createMarkers();
-});
-
-var markerColor = '#2567d5';
-
-var addMarker = function(point, text) {
-	// Marker itself
-	var marker = new fabric.Path('m 11,-19.124715 c -8.2234742,0 -14.8981027,-6.676138 -14.8981027,-14.9016 0,-5.633585 3.35732837,-10.582599 6.3104192,-14.933175 C 4.5507896,-52.109948 9.1631953,-59.34619 11,-61.92345 c 1.733396,2.518329 6.760904,9.975806 8.874266,13.22971 3.050966,4.697513 6.023837,8.647788 6.023837,14.667425 0,8.225462 -6.674629,14.9016 -14.898103,14.9016 z m 0,-9.996913 c 2.703016,0 4.903568,-2.201022 4.903568,-4.904687 0,-2.703664 -2.200552,-4.873493 -4.903568,-4.873493 -2.7030165,0 -4.903568,2.169829 -4.903568,4.873493 0,2.703665 2.2005515,4.904687 4.903568,4.904687 z"', 
-	{
-		width: 40, 
-		height: 80,
-		scaleX: scale, 
-		scaleY: scale, 
-		left: point.x,
-		top: point.y,
-		originX: 'center',
-		originY: 'center',
-		fill: markerColor,
-		stroke: '#2e69b6',
-		text: text // save text inside the marker for import/export 
-	}),
-	// Text
-	textObject = new fabric.Text(text, { 
-		fontSize: 30, 
-		originX: 'center', 
-		fill: markerColor,
-		originY: 'center' 
-	}),
-	// Wrapper
-	background = new fabric.Rect({
-		width: 100, 
-		height: 40, 
-		originX: 'center', 
-		originY: 'center',
-		fill: 'white',
-		stroke: 'black'
-	}),
-	// Group for correct positioning
-	textGroup = new fabric.Group([background, textObject], { 
-		scaleX: scale,
-		scaleY: scale,
-		left: point.x + 20 * scale, // respect current scale
-		top: point.y - 30 * scale // respect current scale
+		}
 	});
 
-	canvas.add(marker);
-	canvas.add(textGroup);
+
+	Markers.push(marker);
+}
+//END:Knapheidesi Marker hinzufügen 
+
+var firstLoad = function () {
+	context.font = "15px Georgia";
+	context.textAlign = "center";
+	getthemarkerdatat();
+}
+
+//END:Knapheidesi hier muss abgefragt werden ob schon bearbeitet, oder wo anders einbinden, dass Bild geändert werden kann 
+
+var firstLoad = function () {
+	context.font = "15px Georgia";
+	context.textAlign = "center";
+	getthemarkerdatat();
+}
+
+firstLoad();
+
+var main = function () {
+	draw();
 };
 
 
-addMarker({x: 550, y: 390}, '#0:500');
-	addMarker({x: 460, y: 120}, '#1:300');
-	canvas.renderAll();
-	
-	
+var draw = function () {
+	console.log(Markers);
+	//Clear Canvas
+	context.fillStyle = "#000";
+
+	context.fillRect(0, 0, canvas.width, canvas.height);
+
+	// Draw map
+	// Sprite, X location, Y location, Image width, Image height
+	// You can leave the image height and width off, if you do it will draw the image at default size
+	context.drawImage(mapSprite, 0, 0, canvas.width, canvas.height);
+
+	// Marker.Sprite = new Image();
+	// Marker.Sprite.src = "http://www.clker.com/cliparts/w/O/e/P/x/i/map-marker-hi.png"
+	// context.drawImage(Marker.Sprite, canvas.width * 0.25, canvas.height * 0.25, canvas.width * 0.048, canvas.height * 0.08);
+
+	// Draw markers
+	for (var i = 0; i < Markers.length; i++) {
+		var tempMarker = Markers[i];
+		// Draw marker
+		context.drawImage(tempMarker.Sprite, tempMarker.XPos * canvas.width, tempMarker.YPos * canvas.height, canvas.width * 0.048, canvas.height * 0.08);
+
+		// // Calculate postion text
+		// var markerText = "Postion (X:" + tempMarker.XPos + ", Y:" + tempMarker.YPos;
+
+		// // Draw a simple box so you can see the position
+		// var textMeasurements = context.measureText(markerText);
+		// context.fillStyle = "#666";
+		// context.globalAlpha = 0.7;
+		// context.fillRect(tempMarker.XPos - (textMeasurements.width / 2), tempMarker.YPos - 15, textMeasurements.width, 20);
+		// context.globalAlpha = 1;
+
+		// // Draw position above
+		// context.fillStyle = "#000";
+		// context.fillText(markerText, tempMarker.XPos, tempMarker.YPos);
+	}
+};
+setInterval(main, (1000 / 100)); // Refresh 100 times a second
